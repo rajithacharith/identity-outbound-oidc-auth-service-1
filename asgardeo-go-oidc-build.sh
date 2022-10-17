75ec8c6bbddc1ae220d0f54b24351cea1183ccb4
rajithacharith <rajithacharith@gmail.com>
2022-10-17 12:10:00 +0530

    add seperate script

---
+++ asgardeo-go-oidc-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Fail the script when a subsuquent command or pipe redirection fails
+set -e
+set -o pipefail
+
+# Variables
+REPO_DIR=$1
+GIT_TOKEN=$2
+cd $REPO_DIR
+VERSION=$(echo $(git describe --tags --abbrev=0))
+ASGARDEO_DOCS_NAME=asgardeo-docs-$VERSION
+GIT_USERNAME='wso2-iam-cloud-bot'
+
+# Check relevant packages are available
+command -v jq >/dev/null 2>&1 || { echo >&2 "Error: $0 script requires 'jq' for JSON Processing.  Aborting as not found."; exit 1; }
+command -v gh >/dev/null 2>&1 || { echo >&2 "Error: $0 script requires 'gh' to call GitHub APIs.  Aborting as not found."; exit 1; } 
