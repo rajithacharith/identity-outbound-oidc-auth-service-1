b6b40fcae5fd636e23dd166368f5ff1b9bce5c64
rajithacharith <rajithacharith@gmail.com>
2022-10-17 15:20:03 +0530

    change release name

--- asgardeo-go-oidc-build.sh
+++ asgardeo-go-oidc-build.sh
@@ -30,7 +30,7 @@ go build -o outbound-server server/server.go
 
 echo "Creating release for version - "$VERSION
 echo $GIT_TOKEN | gh auth login --with-token
-gh release create --title "GO OIDC -"$VERSION outbound-server
+gh release create --title $VERSION outbound-server
 
 # Method to increment minor version
 incrementPackVersion() {
