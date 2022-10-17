3a4e1464f85b91e215512ddc321d047185dac47e
rajithacharith <rajithacharith@gmail.com>
2022-10-17 12:33:05 +0530

    add update version impl

--- asgardeo-go-oidc-build.sh
+++ asgardeo-go-oidc-build.sh
@@ -7,13 +7,11 @@ set -o pipefail
 # Variables
 REPO_DIR=$1
 GIT_TOKEN=$2
+
 git fetch --tags
 # This suppress an error occurred when the repository is a complete one.
 git fetch --prune --unshallow || true
-latest_tag=$(git describe --abbrev=0 --tags || true)
-echo latest_tag
-echo $(ls)
-echo $(git describe --tags --abbrev=0 --always)
+
 cd $REPO_DIR
 echo $(ls)
 VERSION=$(echo $(git describe --tags --abbrev=0 --always))
@@ -23,3 +21,11 @@ GIT_USERNAME=''
 # Check relevant packages are available
 command -v jq >/dev/null 2>&1 || { echo >&2 "Error: $0 script requires 'jq' for JSON Processing.  Aborting as not found."; exit 1; }
 command -v gh >/dev/null 2>&1 || { echo >&2 "Error: $0 script requires 'gh' to call GitHub APIs.  Aborting as not found."; exit 1; } 
+
+incrementPackVersion() {
+    old_version=$1
+    echo "$old_version" | awk -F. '{print $1"."$2"."$3+1}'
+}
+
+NEW_ASGARDEO_DOCS_VERSION=$(incrementPackVersion "$VERSION")
+echo "Next asgardeo-docs version: $NEW_ASGARDEO_DOCS_VERSION"
