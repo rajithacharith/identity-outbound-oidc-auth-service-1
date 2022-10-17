c3f197df5d388bab70ef808da35ebadb36280fb4
rajithacharith <rajithacharith@gmail.com>
2022-10-17 14:30:26 +0530

    add version.txt

--- asgardeo-go-oidc-build.sh
+++ asgardeo-go-oidc-build.sh
@@ -8,19 +8,13 @@ set -o pipefail
 REPO_DIR=$1
 GIT_TOKEN=$2
 
-git fetch --tags
-# This suppress an error occurred when the repository is a complete one.
-git fetch --prune --unshallow || true
-
-cd $REPO_DIR
-echo $(ls)
-VERSION=$(echo $(git describe --tags --abbrev=0 --always))
-ASGARDEO_DOCS_NAME=asgardeo-docs-$VERSION
-GIT_USERNAME=''
-
 # Check relevant packages are available
 command -v jq >/dev/null 2>&1 || { echo >&2 "Error: $0 script requires 'jq' for JSON Processing.  Aborting as not found."; exit 1; }
 command -v gh >/dev/null 2>&1 || { echo >&2 "Error: $0 script requires 'gh' to call GitHub APIs.  Aborting as not found."; exit 1; } 
+
+VERSION=$(cat version.txt)
+echo $VERSION
+GIT_USERNAME='wso2-iam-cloud-bot'
 
 incrementPackVersion() {
     old_version=$1
@@ -29,3 +23,13 @@ incrementPackVersion() {
 
 NEW_ASGARDEO_DOCS_VERSION=$(incrementPackVersion "$VERSION")
 echo "Next asgardeo-docs version: $NEW_ASGARDEO_DOCS_VERSION"
+
+sed -i 's|'"${VERSION}"'|'"${NEW_ASGARDEO_DOCS_VERSION}"'|' version.txt
+
+git -C "$REPO_DIR" config user.name "$GIT_USERNAME"
+git -C "$REPO_DIR" config user.email "iam-cloud@wso2.com"
+git -C "$REPO_DIR" pull
+
+git -C "$REPO_DIR" add "version.txt"
+git -C "$REPO_DIR" commit -m "Updating version to $NEW_ASGARDEO_DOCS_VERSION"
+git -C "$REPO_DIR" push
