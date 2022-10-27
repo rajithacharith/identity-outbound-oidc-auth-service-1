7f41e06730b78b2214dba4948ab6aae617c17f99
rajithacharith <rajithacharith@gmail.com>
2022-10-27 12:10:04 +0530

    update

--- asgardeo-go-oidc-build.sh
+++ asgardeo-go-oidc-build.sh
@@ -52,6 +52,6 @@ git -C "$REPO_DIR" pull
 
 git -C "$REPO_DIR" add "version.txt"
 git -C "$REPO_DIR" commit -m "Updating version to $NEW_OIDC_OUTBOUND_GO_SERVER_VERSION"
-git push
+git push origin temporary-version-bump
 
 gh pr create --fill 
